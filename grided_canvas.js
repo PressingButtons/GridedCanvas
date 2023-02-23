b601d36ae001b6daaf1039a78b9f3f3121a04d4a
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2023-02-23 11:05:14 -0600

    Fixes after initial test, createHTML script and addition of further customization getters and setters

--- grided_canvas.js
+++ grided_canvas.js
@@ -10,10 +10,22 @@ export default class GridedCanvas extends EventTarget {
     constructor(width, height, namespace = "http://www.w3.org/2000/svg") {
         super( );
         this.#namespace = namespace;
+        this.#body.style.position = 'relative';
+        this.#body.style.display = "inline-block";
         this.#body.innerHTML = createHTML(namespace);
         this.width = width;
         this.height = height;
-    } 
+        this.#setListeners( );
+    }
+    
+    #mouseEvent(event) {
+        const rect = this.#body.getBoundingClientRect( );
+        const x = parseInt(event.clientX - rect.x);
+        const y = parseInt(event.clientY - rect.y);
+        const span = this.#body.querySelector('span');
+        span.innerHTML = `[${x}px, ${y}px]`
+        span.style.transform = `translate(${x}px, ${y}px)`;
+    }
 
     /**
      * 
@@ -22,8 +34,20 @@ export default class GridedCanvas extends EventTarget {
      */
     #setAttributes(svg, options) {
         for(const option in options) {
-            svg.setAttribute(option, ioptions[option]);
+            svg.setAttribute(option, options[option]);
         }
+    }
+
+    #setListeners( ) {
+        this.#body.addEventListener('mousemove', this.#mouseEvent.bind(this));
+        this.#body.addEventListener('mouseenter', this.#mouseEvent.bind(this));
+        this.#body.addEventListener('mouseleave', event => {
+            this.body.querySelector('span').innerHTML = '';
+        })
+    }
+
+    get body( ) {
+        return this.#body
     }
 
     get canvas( ) {
@@ -31,7 +55,7 @@ export default class GridedCanvas extends EventTarget {
     }
 
     get width( ) {
-        return this.#canvas.width;
+        return this.canvas.width;
     }
 
     set width(n) {
@@ -90,18 +114,19 @@ export default class GridedCanvas extends EventTarget {
 
 const createHTML = (namespace = "http://www.w3.org/2000/svg") => {
     return `
-        <svg viewBox="0 0 350 150" xlmns="${namespace}" style="position: absolute; width: 100%, height: 100%">
+        <svg viewBox="0 0 350 150" xlmns="${namespace}" style="position: absolute; width: 100%; height: 100%">
             <defs>
                 <pattern id ="micro-grid" width="8" height="8" patternUnits="userSpaceOnUse">
-                    <path d="M 8 0 L 0 0 0 8" fill="none" stroke="rgba(0, 0, 0, 0.4)" stroke-width="0.5"/>
+                    <path d="M 8 0 L 0 0 0 8" fill="none" stroke="rgba(0, 0, 0, 0.6)" stroke-width="0.5"/>
                 </pattern>
                 <pattern id="macro-grid" width="32" height="32" patternUnits="userSpaceOnUse">
                     <rect width="32" height="32" fill="url(#micro-grid)"/>
-                    <path d="M 32 0 L 0 0 0 32" fill="none" stroke="rgba(0, 0, 0, 0.8) stroke-width="1"/>
+                    <path d="M 32 0 L 0 0 0 32" fill="none" stroke="rgba(0, 0, 0, 1)" stroke-width="1"/>
                 </pattern>
             </defs>
-            <rect width="100%" heigth="100%" fill="url(#macro-grid)"/>
+            <rect width="100%" height="100%" fill="url(#macro-grid)"/>
         </svg>
-        <canvas></canvas>
+        <canvas style="display:block;"></canvas>
+        <span class='coordinator' style="position: absolute; top: 20px; left: 20px; user-select: none; background: #EEA; color: black; border-radius: 5px; padding: 5px"></span>
     `
 }
